bbf1fda582274bd1449b564a6b6b67be1ee08b53
Brochb <brochbaltzer@gmail.com>
2023-11-28 10:21:16 -0700

    removed dist, added dependencies, updated resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,16 +1,36 @@
-// Example resolver functions
+const { User, Books } = require('../models');
+const { authMiddleware, signToken } = require('../utils/auth');
+
 const resolvers = {
     Query: {
-        me: (_, args, context) => {
-            // Implement your logic to return a User type
+        me: async (_, args, context) => {
+            // Use authMiddleware to check for authentication
+            const currentUser = await authMiddleware(context);
+
+            if (!currentUser) {
+                throw new Error('Not authenticated');
+            }
+
+            return currentUser;
         },
     },
     Mutation: {
-        login: (_, { email, password }) => {
+        login: async (_, { email, password }) => {
             // Implement your logic to authenticate and return an Auth type
+            const user = await User.findOne({ email });
+
+            if (!user || !(await user.isCorrectPassword(password))) {
+                throw new Error('Invalid email or password');
+            }
+
+            const token = signToken(user);
+            return { token, user };
         },
-        addUser: (_, { username, email, password }) => {
+        addUser: async (_, { username, email, password }) => {
             // Implement your logic to add a user and return an Auth type
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
         },
         // Implement other mutation functions
     },
