0ecd62ab671af76a059bca7a3ede97c9e8319d32
Brochb <brochbaltzer@gmail.com>
2023-11-29 18:48:32 -0700

    created mutations.js, and queries.js, deleted old utils files

---
+++ client/src/utils/queries.js
@@ -0,0 +1,14 @@
+// queries.js
+
+export const getMeQuery = (token) => {
+    return fetch('/api/users/me', {
+        headers: {
+            'Content-Type': 'application/json',
+            authorization: `Bearer ${token}`,
+        },
+    });
+};
+
+export const searchGoogleBooksQuery = (query) => {
+    return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
+};
